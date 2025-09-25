cd42f04e9be09d99e929f9b1e1811b64f883ce3b
Michael lemito <delovoy.lemit@gmail.com>
2025-09-25 21:04:24 +0300

    добавил чтоб смотреть

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,10 +2,12 @@
 import dynamic from 'next/dynamic';
 
 const Dashboard = dynamic(() => import('@/components/Dashboard'), {ssr: false});
+const CTGStreamComponent = dynamic(() => import('@/components/CTGStream')); //, {ssr: false}
 
 export default function Home() {
     return (
         <>
+            <CTGStreamComponent />
             <Dashboard />
         </>
     );
