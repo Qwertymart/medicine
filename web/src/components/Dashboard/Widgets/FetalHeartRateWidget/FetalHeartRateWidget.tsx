2cdb97fc5e7d5ec4b57c21e19e13bec88bddf7b2
Michael lemito <delovoy.lemit@gmail.com>
2025-09-29 18:40:55 +0300

    соединил графики

--- web/src/components/Dashboard/Widgets/FetalHeartRateWidget/FetalHeartRateWidget.tsx
+++ web/src/components/Dashboard/Widgets/FetalHeartRateWidget/FetalHeartRateWidget.tsx
@@ -1,23 +1,24 @@
-import {Card, Text} from '@gravity-ui/uikit';
-import {useSession} from '../../SessionContext';
 import {Graphs} from '../../../Graphs';
 import {Wrapper} from '../Wrapper';
 
 export const FetalHeartRateWidget = () => {
-    const {activeSession} = useSession();
-
     return (
         <Wrapper>
-            <Text variant="header-2" style={{marginBottom: '10px'}}>
-                ЧСС плода
-            </Text>
-            {activeSession && (
-                <Card view="filled" style={{padding: '10px', marginBottom: '10px'}}>
-                    <Text variant="body-2">Card ID: {activeSession.card_id}</Text>
-                </Card>
-            )}
             <div>
-                <Graphs dataType="fetal_heart_rate" title="ЧСС плода (уд/мин)" color="#6c59c2" />
+                <div style={{paddingBottom: '1%'}}>
+                    <Graphs
+                        dataType="fetal_heart_rate"
+                        title="ЧСС плода (уд/мин)"
+                        color="#6c59c2"
+                    />
+                </div>
+                <div>
+                    <Graphs
+                        dataType="uterine_contractions"
+                        title="Сокращения матки"
+                        color="#ff2d87"
+                    />
+                </div>
             </div>
         </Wrapper>
     );
